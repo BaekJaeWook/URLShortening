4be8966d9b524b8cd8d8c1a183ae180105c45fb1
Ian J.Um <31336310+code0xff@users.noreply.github.com>
2018-06-02 14:34:38 +0900

    add new file

---
+++ src/main/java/com/example/demo/UrlMap.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+public class UrlMap {
+	private String shorteningKey;
+	private String longUrl;
+	private String createDate;
+
+	public String getShorteningKey() {
+		return shorteningKey;
+	}
+
+	public void setShorteningKey(String shorteningKey) {
+		this.shorteningKey = shorteningKey;
+	}
+
+	public String getLongUrl() {
+		return longUrl;
+	}
+
+	public void setLongUrl(String longUrl) {
+		this.longUrl = longUrl;
+	}
+
+	public String getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(String createDate) {
+		this.createDate = createDate;
+	}
+}
